8de082b146f374b81d59be0d403f78b356a79303
Fouquet <sylwia.maciulewicz@gmail.com>
2016-12-13 01:35:42 +0100

    fixes2

--- pages/ankieta.php
+++ pages/ankieta.php
@@ -1,7 +1,11 @@
+<!DOCTYPE html>
 <html>
-   <head>
+   <head lang="pl">
       <meta charset = "utf-8">
-      <title>Form Validation</title>
+      <title>Ankieta - walidacja formularza!</title> 
+		<meta name="keywords" content="ankieta, dodatkowa ankieta, pytania, odpowiedz">
+		<meta name="description" content="Dodatkowa aniketa dzięki której poznamy wasze zainteresowania!">
+		<meta name="author" content="Sylwia i Hanna">
       <style type = "text/css">
          p       { margin: 0px; }
          .error  { color: red }
@@ -19,18 +23,22 @@
                http://www.yourtextornumbershereatleast3.com/</p><p> 
               wpisz poprawny url i wyslij jeszcze raz.</p>
                <p>Dzieki.</p></body></html>" );
-            die("BYE");
+            die();
          }
 		 ?>
 	  <p class = "head">Super - podales prawidlowy url</p>
 	  <?php
+		
+		// adress		
 		 print("<p> Adres IP odwiedziajacego: ");
 		print($_SERVER['REMOTE_ADDR']);
 		print("</p>");
 		
+		
+		//regular expressions
         $search = $_POST["longtxt"];
 		$ending = $_POST["ending"];
-		print("<p>$search</p>")
+		print("<p>$search</p>");
          print( "<p>Slowa konczace sie na $ending: " );
 
          while ( preg_match( "/\b([a-zA-Z]*$ending)\b/i", $search, $match ) )
@@ -41,7 +49,29 @@
          } 
 
          print( "</p>" );
-        
+		 
+		 //foreach and string operations
+        $myDictionary = array(  
+            "January"   => "Styczen",   "February" => "Luty",
+            "March"     => "Marzec",   "April"    => "Kwiecien",
+            "May"       => "Maj",   "June"     => "Czerwiec",
+            "July"      => "Lipiec", "August"   => "Sierpien",
+            "September" => "Wrzesien",   "October"  => "Pazdziernik",
+            "November"  => "Listopad","December" => "Grudzien",
+			"Zebra"		=>	"Zebra");
+
+         // print each element’s name and value
+         foreach ( $myDictionary as $engKey => $plVal ){
+            print( "<p>Angielskie slowo: '$engKey' po polsku znaczy '$plVal'</p>" );
+			 
+			if ( strcmp( $engKey, $plVal ) < 0 )
+               print( "<p> Uzywam strcmp" . $engKey . " bedzie wczesniej niz " . $plVal);
+            elseif ( $engKey > $plVal ) 
+               print( "<p> Uzywam relacyjnego >" . $engKey . " bedzie pozniej niz " . $plVal);
+            else 
+               print( "<p>" . $engKey . " jest takie same jak " . $plVal );
+			
+		 }
       ?>
    </body>
 </html>
